cd54a2d9b8a9ed53c370bb99dbabcf236f541a51
tanarakchuns <tanarak.chuns@gmail.com>
2023-01-16 16:08:07 +0700

    add Home

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,14 +1,42 @@
 import Container from "react-bootstrap/esm/Container";
-import Carousel from 'react-bootstrap/Carousel';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 import "./Home.css"
+import HomeCard from "./HomeCard";
 
 const Home = () => {
-    return (<Container>
+    return (
         <div className="banner">
-             <button className="btn btn-success" type="button">Button</button>
+            <Row>
+                <Col>
+                    <div className="banner-left">
+                        <h1><b>Come Fly With Me</b></h1>
+                        <h3>Come Fine with me</h3>
+                    </div>
+                </Col>
+                <Col>
+                    <div className="banner-right">
+                        <button className="btn btn-success" type="button">Buy now</button>
+                    </div>
+                </Col>
+            </Row>
+            <br/>
+            <Container>
+                <h2>Plans</h2>
+                <Row>
+                        <HomeCard/>
+                        <HomeCard/>
+                        <HomeCard/>
+                        <HomeCard/>
+                </Row>
+                <h2>Ads</h2>
+                <Row>
+                         <img src="https://www.wordstream.com/wp-content/uploads/2021/07/persuasive-ads-coca-cola-1.jpg" alt="" />
+                </Row>
+            </Container>
         </div>
-    </Container>  );
+      );
 }
  
 export default Home;
